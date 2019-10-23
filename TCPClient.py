8c802293e737411eb78c8dad978c7ce99206cdbe
kedar-shenoy9 <kedarshenoy.cs17@bmsce.ac.in>
2019-10-23 12:38:10 +0530

    socket-programmin-2

---
+++ TCPClient.py
@@ -0,0 +1,12 @@
+from socket import *
+serverPort = 12000
+serverName = '10.124.6.94'
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+fileName = input("Enter file name: ")
+print("Establishing connection ...")
+print("File contents:")
+clientSocket.send(fileName.encode())
+fileContents = clientSocket.recv(1024).decode()
+print('\n', str(fileContents), '\n')
+clientSocket.close()
